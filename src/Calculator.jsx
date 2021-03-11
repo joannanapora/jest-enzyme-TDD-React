b97d8171c75a131e4fb28ebc64a799498b4a3fe7
Joanna Napora <joannanapora@Joannas-MacBook-Air.local>
2021-03-11 15:42:21 +0000

    feat: added functionality

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -5,28 +5,71 @@ import Keypad from './Keypad';
 
 class Calculator extends Component {
    state = {
-    // value to be displayed in <Display />
     displayValue: '0',
-    // values to be displayed in number <Keys />
-    numbers: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '.', '0','ce'],
-    // values to be displayed in operator <Keys />
-    operators: ['/', 'x', '-', '+'],
-    // operator selected for math operation
+    numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'c', '0','.'],
+    operators: ['+', '-', 'x', '/'],
     selectedOperator: '',
-    // stored value to use for math operation
     storedValue: '',
   }
 
   callOperator = () => {
-    console.log('call operation');
+    let { displayValue, selectedOperator, storedValue } = this.state;
+ 
+    const updateStoredValue = displayValue;
+    displayValue = parseInt(displayValue, 10);
+    storedValue = parseInt(storedValue, 10);
+
+    switch (selectedOperator) {
+      case '+':
+        displayValue = storedValue + displayValue;
+        break;
+      case '-':
+        displayValue = storedValue - displayValue;
+        break;
+      case 'x':
+        displayValue = storedValue * displayValue;
+        break;
+      case '/':
+        displayValue = storedValue / displayValue;
+        break;
+      default:
+        displayValue = '0';
+    }
+
+    displayValue = displayValue.toString();
+    selectedOperator = '';
+    if (displayValue === 'NaN' || displayValue === 'Infinity') displayValue = '0';
+
+    this.setState({ displayValue, selectedOperator, storedValue: updateStoredValue });
   }
 
-  setOperator = () => {
-    console.log('set operation');
+  setOperator = value => {
+    let { displayValue, selectedOperator, storedValue } = this.state;
+
+    if (selectedOperator === '') {
+      storedValue = displayValue;
+      displayValue = '0';
+      selectedOperator = value;
+    } else {
+      selectedOperator = value;
+    }
+
+    this.setState({ displayValue, selectedOperator, storedValue });
   }
 
-  updateDisplay = () => {
-    console.log('update display');
+  updateDisplay = value => {
+    let { displayValue } = this.state;
+    if (value === '.' && displayValue.includes('.')) {
+      value = ''
+    };
+    if (value === 'c') {
+      displayValue = displayValue.substr(0, displayValue.length - 1);
+      if (displayValue === '') {
+        displayValue = '0'};
+    } else {
+      displayValue === '0' ? displayValue = value : displayValue += value}
+
+    this.setState({ displayValue });
   }
 
   
